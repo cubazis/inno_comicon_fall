cb11b0354842c0e5ab80de528182e9216608fbf7
Aidar Valeev <aidarvaleev1998@gmail.com>
2018-09-10 13:56:27 +0300

    laba

---
+++ gradings/labs/valeev-aidar/lang_m/Readmi.md
@@ -0,0 +1,8 @@
+# Assignment 1
+Code written on C#.
+
+### Instruction
+- fill "in.txt" with other input if needed
+- execute commands:
+-- docker build -t cchw1 .
+-- docker run cchw1 cat out.txt > out.txt
---
+++ gradings/labs/valeev-aidar/lang_m/src/Demo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace src
+{
+    class Demo
+    {
+        private const string InputFilePath = @"../../../in.txt";
+        private const string OutputFilePath = @"../../../out.txt";
+
+        private static void Main(string[] args)
+        {
+            try
+            {
+                var input = Read();
+
+                var parcer = new Parcer(input);
+                parcer.Statements();
+
+                Write(parcer.ParcerResult);
+            }
+            catch (FileNotFoundException)
+            {
+                Write("File in.txt was not found.");
+            }
+        }
+
+        private static string Read()
+        {
+            return File.ReadAllText(InputFilePath).Replace("\\n","\n");
+        }
+
+        private static void Write(string text)
+        {
+            File.WriteAllText(OutputFilePath, text);
+        }
+    }
+
+}
