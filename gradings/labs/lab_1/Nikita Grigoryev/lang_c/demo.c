bed4afb117736006c55ccff5c9c13e3c4f56a1ec
nekitboy <n.grigoryev@innopolis.ru>
2018-09-10 15:58:33 +0300

    lab1 solution
    Lab1 solution by Nikita Grigoryev

---
+++ gradings/labs/lab_1/Nikita Grigoryev/lang_c/demo.c
@@ -0,0 +1,19 @@
+//
+// Created by cubazis on 01.09.18.
+//
+
+#include <stdio.h>
+#include "src/parcer.h"
+
+int main(void)
+{
+	file = fopen( "in.txt" , "r");
+	FILE * out = fopen( "out.txt" , "w");
+	statements();
+
+	printf("%s\n", parser_result);
+	fprintf(out, parser_result);
+
+	fclose(file);
+	fclose(out);
+}
